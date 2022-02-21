393a475bc0ca52d5cf6a3566f286b27352491f94
Jacob Manley <jbmanley45@gmail.com>
2022-02-21 15:07:09 -0800

    commit tree a11b87f8112836a4da7c14f3040ce286abe02147

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrray) {
+    let array = ["Antonia", "Nuru", "Sam", "Frodo"]
+    return ["Antonia", "Nuru"]
+}
+const returnLastTwoDrivers = function (array2) {
+    let array1 = ["Antonia", "Nuru", "Amari", "Mo"]
+    return ["Amari", "Mo"]
+}
+const createFareMultiplier = x => {
+    return function (fare) {
+        return fare * x;
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) =>{
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
